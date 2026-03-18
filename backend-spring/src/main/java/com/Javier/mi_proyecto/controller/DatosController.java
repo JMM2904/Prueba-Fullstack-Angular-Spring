4c7664316654d343859817e1d30afa82345ed3da
JMM2904 <javiermartinmunoz853@gmail.com>
2026-03-18 19:30:25 +0100

    Update DatosController.java

--- backend-spring/src/main/java/com/Javier/mi_proyecto/controller/DatosController.java
+++ backend-spring/src/main/java/com/Javier/mi_proyecto/controller/DatosController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://pruebafullstack.up.railway.app/")
 public class DatosController {
 
     private final DatosService service;
